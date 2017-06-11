406dadd639c5607602569202c6037856676d50dc
Xing Zhou <xing@ohdyno.me>
2017-06-11 07:58:56 -0400

    Add failing, but buildable test for Fraction.

---
+++ src/main/java/me/ohdyno/fractions/Fraction.java
@@ -0,0 +1,14 @@
+package me.ohdyno.fractions;
+
+import lombok.Value;
+
+@Value
+public class Fraction {
+    public Fraction(int numerator, int denominator) {
+
+    }
+
+    public Fraction add(Fraction that) {
+        return null;
+    }
+}
